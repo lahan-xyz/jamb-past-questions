5084de4bcadfcb14ffaeee7a1f1670931067edec
lahan-xyz <engineerlahan@gmail.com>
2026-05-27 15:00:54 +0100

    update

--- src/components/AppView.js
+++ src/components/AppView.js
@@ -8,7 +8,7 @@ const SUBJECTS = ['Mathematics', 'English', 'Chemistry', 'Physics']
 const YEARS = Array.from({ length: 2025 - 1978 + 1 }, (_, i) => 1978 + i)
 
 async function loadSubject(subject, year) {
-  const module = await import(`https://cdn.jsdelivr.net/gh/lahan-xyz/jamb-past-questions@main/src/scraper/past-questions/${subject.toLowerCase()}.min.js`);
+  const module = await import(`../scraper/past-questions/${subject.toLowerCase()}.min.js`);
   return module.default[`year_${year}`] || [];
 }
 
